a203aab6760c5263871b3ed1ec81875fe54a9346
auron <auron7985@gmail.com>
2023-10-09 22:14:34 -0500

    added some resolvers for messages

--- server/schemas/Resolvers/messageResolver.js
+++ server/schemas/Resolvers/messageResolver.js
@@ -2,17 +2,34 @@ const { User, Chat, Message } = require("../../models");
 
 const messageResolvers = {
 Query: {
-    userMessages: async(parent, args) => {
+    userMessages: async (parent, args, context) => {
         const { userId } = args;
+        const { currentUser } = context;
+
+        if( currentUser && currentUser._id.toString() === userId) {
+            try {
+    const userMessages =  await Message.find({ sender: userId});
+    return userMessages;
+   } catch (error) {
+     throw new Error(`Error fetching messages for user ${userId}: ${error.message}`);
+   } 
+} else {
+    throw new Error("You are not the authorized user");
+   }
+}
+
+},
+    searchMessages: async(parent, args) => {
         try {
-            const messages = await getUserMessages(userId);
+            const messages = await Message.find({ content: {$regex: keyword, $options: "i"} });
             return messages;
-        } catch (error) {
-            throw new Error(`Error fetching messages for user ${userId}: ${error.message}`);
+        }  catch (error) {
+            throw new Error(`Error searching messages: ${error.message}`)
         }
-    },
-  },
-};
+    }
+    
+  };
+
 
 
 
