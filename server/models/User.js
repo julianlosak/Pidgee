2795c5f41a89c41eca2ddf2b0386d5e628f1c7a8
auron <auron7985@gmail.com>
2023-10-10 20:19:00 -0500

    added more resolvers to messages

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const { Schema, model} = require("mongoose");
-// const bcrypt = require("bcrypt");
-const argon2 = require("argon2");
+const bcrypt = require("bcrypt");
+
 
 const userSchema = new Schema({
     username: {
@@ -31,33 +31,20 @@ const userSchema = new Schema({
      },
 });
 
-userSchema.pre("save", async function (next) {
-    if (this.isNew || this.isDirectModified("password")) {
-      try {
-         const hash = await argon2.hash(this.password);
-         this.password = hash;
-         next();
-      } catch (error) {
-         next(error);
-      }
-      //   const saltRounds = 10;
-      //   this.password = await bcrypt.hash(this.password, saltRounds);
-    } else {
-      next();
-    }
-});
+userSchema.pre('save', async function (next) {
+   if (this.isNew || this.isModified('password')) {
+     const saltRounds = 10;
+     this.password = await bcrypt.hash(this.password, saltRounds);
+   }
+ 
+   next();
+ });
+ 
 
-userSchema.methods.isCorrectPassword = async function (password) {
-   try {
-      return await argon2.verify(this.password, password);
-   } catch (error) {
-     return false;
-
-      }
-   }; 
-   //  return bcrypt.compare(password, this.password);
-// };
-
+ userSchema.methods.isCorrectPassword = async function (password) {
+   return bcrypt.compare(password, this.password);
+ };
+ 
 const User = model("User", userSchema);
 
 module.exports = User;
