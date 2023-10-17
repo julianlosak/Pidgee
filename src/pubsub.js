01d2a58e05e07787f8fe87edf427da51659e393c
julianlosak <julianlosak@gmail.com>
2023-10-16 19:00:21 -0500

    Installed socket.io for functionallity with pubsub.js and to work with zeroMQ

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,4 +1,6 @@
 const zmq = require('zeromq');
+const http = require('http');
+const io = require('socket.io');
 
 const publisher = zmq.socket('pub');
 publisher.bindSync('tcp://127.0.0.1:5556');
@@ -7,14 +9,19 @@ publisher.bindSync('tcp://127.0.0.1:5556');
 function sendChatMessage(topic, message) {
   publisher.send([topic, message]);
 }
-// Usage: sendChatMessage('general', 'Hello, World!');
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-  
-    // Send messages to specific topic via ZeroMQ
-    socket.on('sendMessage', ({ topic, message }) => {
-      sendChatMessage(topic, message);
-    });
+const server = http.createServer();
+const socketServer = io(server);
+
+socketServer.on('connection', (socket) => {
+  console.log('a user connected');
+
+  // Send messages to specific topic via ZeroMQ
+  socket.on('sendMessage', ({ topic, message }) => {
+    sendChatMessage(topic, message);
   });
-  
+});
+
+server.listen(3000, () => {
+  console.log('Socket.io server listening on port 3000');
+});
