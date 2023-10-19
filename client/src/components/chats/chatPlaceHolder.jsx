2333b2fb40081be1ddaee14183753134b74dc09d
TheIanAnderson <131201726+TheIanAnderson@users.noreply.github.com>
2023-10-19 16:38:15 -0500

    Added features where a placeholder directs users to search for a user, added a All search parameter to pull up all users

---
+++ client/src/components/chats/chatPlaceHolder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatPlaceholder = () => {
+  return (
+    <div className="chat-placeholder">
+      <p>Search for a user to begin chatting!</p>
+    </div>
+  );
+};
+
+export default ChatPlaceholder;
