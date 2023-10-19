99909a81b9bc9e3827b94393303ebd5743f93c44
auron <auron7985@gmail.com>
2023-10-19 16:33:41 -0500

    trying to connect mongoose to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pidgee-app');
+mongoose.connect(process.env.MONGODB_URI, {useNewURLParser: true, useUnifiedTopology: true});
 
 module.exports = mongoose.connection;
